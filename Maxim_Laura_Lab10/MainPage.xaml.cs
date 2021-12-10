b2ef2711d573319bb7de50eb4011bddc2ef2682a
Laura Daniela Maxim <Maxim.Da.Laura@student.utcluj.ro>
2021-12-10 13:02:07 +0200

    Add project files.

---
+++ Maxim_Laura_Lab10/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Maxim_Laura_Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
